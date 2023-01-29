584402dd2d456abd43893675a90c8b5d4d821704
Firmware-Repairman <firmware.repairman@gmail.com>
2023-01-29 04:55:02 -0800

    Comment sorting added. As yet untested

--- updownupdown.php
+++ updownupdown.php
@@ -7,26 +7,26 @@
  * Author: Dave Konopka, Martin Scharm
  * Author URI: http://davekonopka.com
  * License: GPL2
- * 
+ *
  *	Copyright 2011 Dave Konopka (email : dave.konopka@gmail.com)
- * 
+ *
  *		This program is free software; you can redistribute it and/or modify
- *		it under the terms of the GNU General Public License, version 2, as 
+ *		it under the terms of the GNU General Public License, version 2, as
  *		published by the Free Software Foundation.
- * 
+ *
  *		This program is distributed in the hope that it will be useful,
  *		but WITHOUT ANY WARRANTY; without even the implied warranty of
  *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
  *		GNU General Public License for more details.
- * 
+ *
  *		You should have received a copy of the GNU General Public License
  *		along with this program; if not, write to the Free Software
  *		Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA	02110-1301	USA
- *		
- *		This Wordpress plugin is released under a GNU General Public License, version 2. 
- *		A complete version of this license can be found here: 
+ *
+ *		This Wordpress plugin is released under a GNU General Public License, version 2.
+ *		A complete version of this license can be found here:
  *		http://www.gnu.org/licenses/gpl-2.0.html
- * 
+ *
  *	This plugin was initially developed as a project of Wharton Research Data Services.
 */
 
@@ -34,7 +34,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 {
 	global $updown_db_version;
 	$updown_db_version = "1.1";
-		
+
 	class UpDownPostCommentVotes
 	{
 		function __construct()
@@ -44,8 +44,9 @@ if (!class_exists("UpDownPostCommentVotes"))
 			add_action( 'wp_ajax_nopriv_register_vote', array( &$this, 'ajax_register_vote' ));
 			add_action( 'wp_head', array( &$this, 'add_ajax_url' ));
 			add_action( 'admin_menu', 'updown_plugin_menu' );
+			add_action( 'comments_?', array( &$this, 'sort_comments' ));
 		}
-		
+
 		public function setup_plugin() {
 			global $wpdb;
 			global $updown_db_version;
@@ -56,7 +57,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 					die ("downgrade not supported!!!"); // there is no prev version using this var
 				return;
 			}
-			
+
 			if ( !empty($wpdb->charset) )
 				$charset_collate = "DEFAULT CHARACTER SET ".$wpdb->charset;
 
@@ -109,27 +110,27 @@ if (!class_exists("UpDownPostCommentVotes"))
 
 			# If old style post vote logging table exists, port records over to new logging table
 			if( $wpdb->get_var("SHOW TABLES LIKE '".$wpdb->base_prefix."up_down_post_votes'") == $wpdb->base_prefix."up_down_post_votes" ) {
-				
+
 				// Port post vote logs
 				$result_query = $wpdb->get_results($wpdb->prepare("SELECT * FROM ".$wpdb->base_prefix."up_down_post_votes"));
-				
+
 				foreach ( $result_query as $value ) {
 					$wpdb->insert ($wpdb->base_prefix."up_down_post_vote",
 						array ('post_id' => $value->post_id,
 										'voter_id' => $value->voter_id,
 										'vote_value' => $value->vote_value));
 				}
-				
+
 				//Drop old post log table
 				$wpdb->query( 'DROP TABLE IF EXISTS '.$wpdb->base_prefix."up_down_post_votes");
 			}
 
 			# If old style comment vote logging table exists, port records over to new logging table
 			if( $wpdb->get_var("SHOW TABLES LIKE '".$wpdb->base_prefix."up_down_comment_votes'") == $wpdb->base_prefix."up_down_comment_votes" ) {
-			
+
 				// Port comment vote logs
 				$result_query = $wpdb->get_results($wpdb->prepare("SELECT * FROM ".$wpdb->base_prefix."up_down_comment_votes"));
-				
+
 				foreach ( $result_query as $value ) {
 					$wpdb->insert ($wpdb->base_prefix."up_down_comment_vote",
 						array ('comment_id' => $value->comment_id,
@@ -143,7 +144,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 			}
 
 			update_option ("updown_db_version", $updown_db_version);
-			
+
 			if (!get_option ("updown_guest_allowed"))
 				update_option ("updown_guest_allowed", "not allowed");
 			if (!get_option ("updown_counter_type"))
@@ -153,12 +154,12 @@ if (!class_exists("UpDownPostCommentVotes"))
 			if (!get_option ('updown_votes_text'))
 				update_option ("updown_votes_text", "votes");
 		}
-		
+
 		public function init_plugin()
 		{
 			$this->load_vote_client_resources();
 		}
-		
+
 		public function load_vote_client_resources()
 		{
 			if ( !is_admin() )
@@ -173,17 +174,17 @@ if (!class_exists("UpDownPostCommentVotes"))
 				wp_register_style( 'updownupdown', plugins_url( '/style/updownupdown.css', __FILE__));
 				}
 				wp_enqueue_style( 'updownupdown' );
-				
+
 				 wp_register_script( 'updownupdown',
 						 plugins_url( '/js/updownupdown.js', __FILE__),
 						 array( 'jquery' ),
 						 '1.0' );
 				 wp_enqueue_script('updownupdown');
-			}			
+			}
 		}
 
 		public function add_ajax_url()
-		{ 
+		{
 			echo '<script type="text/javascript">var UpDownUpDown = { ajaxurl: "'.admin_url ('admin-ajax.php').'" };</script>';
 		}
 
@@ -191,19 +192,19 @@ if (!class_exists("UpDownPostCommentVotes"))
 		{
 			return get_option ("updown_guest_allowed") == "allowed";
 		}
-		
+
 		public function get_user_id()
 		{
 			if (is_user_logged_in())
 				return get_current_user_id();
-			
+
 			// guests user-id = md5 hash of it's IP
 			if ($this->guest_allowed())
 				return md5 ($_SERVER['REMOTE_ADDR']);
-			
+
 			return 0;
 		}
-		
+
 		public function get_post_votes_total ( $post_id )
 		{
 			global $wpdb;
@@ -212,8 +213,8 @@ if (!class_exists("UpDownPostCommentVotes"))
 				return false;
 
 			$result_query = $wpdb->get_results($wpdb->prepare("
-															SELECT vote_count_up, vote_count_down 
-															FROM ".$wpdb->base_prefix."up_down_post_vote_totals 
+															SELECT vote_count_up, vote_count_down
+															FROM ".$wpdb->base_prefix."up_down_post_vote_totals
 															WHERE post_id = %d", $post_id));
 
 			return ( count($result_query) == 1 ? array( "up" => $result_query[0]->vote_count_up, "down" => $result_query[0]->vote_count_down ) : array( "up" => 0, "down" => 0 ));
@@ -226,8 +227,8 @@ if (!class_exists("UpDownPostCommentVotes"))
 				return false;
 
 			$result_query = $wpdb->get_results($wpdb->prepare("
-															SELECT vote_count_up, vote_count_down 
-															FROM ".$wpdb->base_prefix."up_down_comment_vote_totals 
+															SELECT vote_count_up, vote_count_down
+															FROM ".$wpdb->base_prefix."up_down_comment_vote_totals
 															WHERE comment_id = %d", $comment_id));
 
 			return ( count($result_query) == 1 ? array( "up" => $result_query[0]->vote_count_up, "down" => $result_query[0]->vote_count_down ) : array( "up" => 0, "down" => 0 ));
@@ -236,7 +237,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 		public function get_post_user_vote( $user_id, $post_id ) {
 			global $wpdb;
 			return $wpdb->get_var($wpdb->prepare("
-													SELECT vote_value 
+													SELECT vote_value
 													FROM ".$wpdb->base_prefix."up_down_post_vote
 													WHERE voter_id =	%s
 													AND post_id = %d", $user_id, $post_id));
@@ -245,14 +246,14 @@ if (!class_exists("UpDownPostCommentVotes"))
 		public function get_comment_user_vote( $user_id, $comment_id ) {
 			global $wpdb;
 			return $wpdb->get_var($wpdb->prepare("
-													SELECT vote_value 
+													SELECT vote_value
 													FROM ".$wpdb->base_prefix."up_down_comment_vote
 													WHERE voter_id =	%s
 													AND comment_id = %d", $user_id, $comment_id));
 		}
 
-		public function render_vote_badge( $vote_up_count = 0, $vote_down_count = 0, $votable = true, $existing_vote = 0 ) { 
-			
+		public function render_vote_badge( $vote_up_count = 0, $vote_down_count = 0, $votable = true, $existing_vote = 0 ) {
+
 			$img_up_status = '';
 			$img_down_status = '';
 			$vote_label = '';
@@ -261,7 +262,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 			$down_classnames = '';
 
 			$votable = (is_user_logged_in() || $this->guest_allowed()) && $votable;
-			
+
 			if ( $existing_vote > 0 )
 				$img_up_status = '-on';
 			elseif ( $existing_vote < 0 )
@@ -272,13 +273,13 @@ if (!class_exists("UpDownPostCommentVotes"))
 
 			$vote_total_count = $vote_up_count - $vote_down_count;
 			$vote_total_count_num = $vote_up_count + $vote_down_count;
-			
+
 			if ( $vote_down_count > 0 )
 			{
 				$vote_down_count *= -1;
 				$down_classnames = ' updown-active';
 			}
-			
+
 			if ( $vote_up_count > 0 ) {
 				$vote_up_count = "+" . $vote_up_count;
 				$up_classnames = ' updown-active';
@@ -288,7 +289,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 				$updown_classnames .= ' updown-count-sign';
 			else
 				$updown_classnames .= ' updown-count-unsign';
-			
+
 			if ($vote_total_count > 0)
 			{
 				if (!get_option ("updown_counter_sign") || get_option ("updown_counter_sign") == "yes")
@@ -303,19 +304,19 @@ if (!class_exists("UpDownPostCommentVotes"))
 				$updown_classnames .= ' updown-neg-count';
 				//				$updown_style = 'style="color:#bb5853" ';
 			}
-			
+
 			if ( $vote_up_count == 0 && $vote_down_count == 0 && $votable )
 			{
 				$vote_up_count = '';
 				$vote_down_count = '';
 				$vote_total_count = '';
 			}
-			
+
 			if ($votable)
 				$vote_label = get_option ('updown_vote_text');
 			else
 				$vote_label .= get_option ('updown_votes_text');
-				
+
 			if ($votable)
 				echo '<div><img class="updown-button updown-up-button" vote-direction="1" src="'.$up_img_src.'"></div>';
 
@@ -333,7 +334,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 				echo '<div><img class="updown-button updown-down-button" vote-direction="-1" src="'.$down_img_src.'"></div>';
 
 			echo '<div class="updown-label">'.$vote_label.'</div>';
-			
+
 		}
 
 		//********************************************************************
@@ -341,12 +342,12 @@ if (!class_exists("UpDownPostCommentVotes"))
 
 		public function ajax_register_vote() {
 			global $wpdb;
-			
-			$result = array( 'status' => '-1', 
-												'message' => '', 
-												'vote_totals' => null, 
-												'post_id' => null, 
-												'comment_id' => null, 
+
+			$result = array( 'status' => '-1',
+												'message' => '',
+												'vote_totals' => null,
+												'post_id' => null,
+												'comment_id' => null,
 												'direction' => null );
 
 			if (!is_user_logged_in() && !$this->guest_allowed())
@@ -354,7 +355,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 				$result['message'] = 'You must be logged in to vote.';
 				die(json_encode($result));
 			}
-			
+
 			$user_id = $this->get_user_id();
 
 			//Validate expected params
@@ -383,7 +384,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 			if ( $vote_value > 0 ) {
 				$vote_value = 1;
 				$up_vote_delta = 1;
-			} elseif ( $vote_value < 0 ) { 
+			} elseif ( $vote_value < 0 ) {
 				$vote_value = -1;
 				$down_vote_delta = 1;
 			}
@@ -392,7 +393,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 				$existing_vote = $this->get_post_user_vote( $user_id, $post_id );
 			elseif	( $element_name == 'comment' )
 				$existing_vote = $this->get_comment_user_vote( $user_id, $comment_id );
-			
+
 			if ( $existing_vote < 0 )
 				$down_vote_delta -= 1;
 			elseif ( $existing_vote > 0 )
@@ -439,8 +440,58 @@ if (!class_exists("UpDownPostCommentVotes"))
 
 			die(json_encode($result));
 		}
+
+		function sort_comments( $post_id, $comments ) {
+			// Port post vote logs
+			$result_query = $wpdb->get_results($wpdb->prepare(
+				"SELECT * FROM ".$wpdb->base_prefix."up_down_comment_vote_totals
+				WHERE post_id=".$post_id));
+
+			$net_votes = array();
+			$num_votes = array();
+			foreach ( $result_query as $value ) {
+				$net_votes[ $value->comment_id ] = $value->vote_count_up - $value->vote_count_down;
+				$num_votes[ $value->comment_id ] = $value->vote_count_up + $value->vote_count_down;
+			}
+
+			arsort( $net_votes );
+			arsort( $num_votes );
+
+			usort( $comments, function( $a, $b ) {
+				$id_a = $a->comment_id;
+				$id_b = $b->comment_id;
+
+				$a_defined = $net_votes[ $id_a ];
+				$b_defined = $net_votes[ $id_b ];
+				if ( ! $a_defined ) {
+					if ( ! $b_defined ) {
+						// neither has comments
+						return 0;
+					} else {
+						// a has no comments, but b does, b goes first
+						return 1;
+					}
+				} else if (! $b_defined ) {
+					// b has no comments, but a does, a goes first
+					return -1;
+				}
+
+				$net_a = $net_votes[ $id_a ];
+				$net_b = $net_votes[ $id_b ];
+				if ( $net_a > $net_b) {
+					// a has net more up votes than b
+					return -1;
+				} else if ( $net_a > $net_b) {
+					// b has net more up votes than a
+					return 1;
+				} else {
+					// same number of net up votes, whichever did it with fewer down votes goes first
+					return $num_votes[ $id_a ] - $num_votes[ $id_b ];
+				}
+			});
+		}
 	} //class:UpDownPostCommentVotes
-	
+
 	//Create instance of plugin
 	$up_down_plugin = new UpDownPostCommentVotes();
 
@@ -459,7 +510,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 
 		if ( !$post_id )
 			return false;
-			
+
 		$vote_counts = $up_down_plugin->get_post_votes_total( $post_id );
 		$existing_vote = $up_down_plugin->get_post_user_vote( $up_down_plugin->get_user_id(), $post_id );
 
@@ -473,23 +524,23 @@ if (!class_exists("UpDownPostCommentVotes"))
 
 		if ( !$comment_id )
 			return false;
-			
+
 		$vote_counts = $up_down_plugin->get_comment_votes_total( $comment_id );
 		$existing_vote = $up_down_plugin->get_comment_user_vote( $up_down_plugin->get_user_id(), $comment_id );
-		
+
 		echo '<div class="updown-vote-box updown-comments" id="updown-comment-'.$comment_id.'" comment-id="'.$comment_id.'">';
 		$up_down_plugin->render_vote_badge( $vote_counts["up"], $vote_counts["down"], $allow_votes, $existing_vote );
 		echo '</div>';
 	}
 
-	
+
 	//********************************************************************
 	// Admin page
-	
+
 	function updown_plugin_menu()
 	{
 		add_options_page('UpDown Options', 'UpDownUpDown', 'manage_options', 'updown_plugin_menu_id', 'updown_options');
-} 
+}
 
 	function updown_options()
 	{
@@ -498,7 +549,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 		{
 			wp_die( __('You do not have sufficient permissions to access this page.') );
 		}
-		
+
 		if (isset ($_POST['Submit']))
 		{
 			// guest allowed
@@ -508,28 +559,28 @@ if (!class_exists("UpDownPostCommentVotes"))
 			}
 			else
 				update_option ("updown_guest_allowed", "not allowed");
-			
+
 			// style
 			update_option ("updown_css", trim (strip_tags ($_POST['style'])));
-			
+
 			// counter type
 			update_option ("updown_counter_type", trim (strip_tags ($_POST['counter'])));
 			if ($_POST['counter-sign'] == "yes")
 				update_option ("updown_counter_sign", "yes");
 			else
 				update_option ("updown_counter_sign", "no");
-			
+
 			// text
 			update_option ("updown_vote_text", trim ($_POST['votetext']));
 			update_option ("updown_votes_text", trim ($_POST['votestext']));
 		}
-		
+
 		echo '<div class="wrap"><h2>UpDownUpDown Plugin Settings</h2><form name="form1" method="post" action=""><table width="100%" cellpadding="5" class="form-table"><tbody>';
-		
+
 		// permissions
 		$allow_guests = get_option ("updown_guest_allowed") == "allowed" ? "checked " : "";
 		echo '<tr valign="top"><th>Allow guests to vote:</th><td><input type="checkbox" name="guest_allowed" '.$allow_guests.'/> <span class="description">Allow guest visitors to vote without login? (Votes tracked by ip address)</span></td></tr>';
-		
+
 		//style
 		$selected = "";
 		echo '<tr valign="top"><th>Badge style:</th><td><select name="style">';
@@ -540,7 +591,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 		else $selected = "";
 		echo '<option value="simple"'.$selected.'>simple</option>';
 		echo '</select> <span class="description">Choose basic badge style. You can also override CSS in your theme.</span></td></tr>';
-		
+
 		// counter type
 		echo '<tr valign="top"><th>Counter type:</th><td>';
 		if (!get_option ("updown_counter_type") || get_option ("updown_counter_type") == "plusminus")
@@ -554,7 +605,7 @@ if (!class_exists("UpDownPostCommentVotes"))
 			$selected = "";
 		echo '<input type="radio" name="counter" value="total" '.$selected.'/> Total ';
 		echo ' <span class="description">Do you want to see the positive and negative counts, or only a total score?</td></tr>';
-		
+
 		// sign?
 		echo '<tr valign="top"><th>Sign total counter:</th><td>';
 		if (!get_option ("updown_counter_sign") || get_option ("updown_counter_sign") == "yes")
@@ -568,17 +619,17 @@ if (!class_exists("UpDownPostCommentVotes"))
 			$selected = "";
 		echo '<input type="radio" name="counter-sign" value="no" '.$selected.'/> don\'t sign ';
 		echo ' <span class="description">Should the total score contain a +/- in front of it?</span></td></tr>';
-		
+
 		//text
 		echo '<tr valign="top"><th>Vote label if voteable:</th><td><input type="text" name="votetext" value="'.get_option('updown_vote_text').'"/> <span class="description">Text on the bottom of the buttons if the visitor is allowed to vote (HTML allowed)</span></td></tr>';
 		echo '<tr valign="top"><th>Vote label if not voteable:</th><td><input type="text" name="votestext" value="'.get_option('updown_votes_text').'"/> <span class="description">Text on the bottom of the buttons if the visitor is <strong>not</strong> allowed to vote (HTML allowed)</span></td></tr>';
-		
+
 		echo '</tbody></table>';
-		
+
 		//save
 		echo '<p class="submit"><input type="submit" name="Submit" class="button-primary" value="'.esc_attr('Save Changes').'" /></p>';
 		echo '</form></div>';
 	}
-} 
+}
 
 ?>
